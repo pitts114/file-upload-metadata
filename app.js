359a5b14d6ebe81936d8e65ccda73ce820badea8
pullman14 <pitts.114@buckeyemail.osu.edu>
2017-08-21 20:40:02 -0400

    Return JSON response
    Filename and file size

--- app.js
+++ app.js
@@ -10,8 +10,11 @@ app.set('port', (process.env.PORT || 5000))
 app.post('/upload', upload.single('userFile'), function (req, res, next) {
   // req.file is the `avatar` file
   // req.body will hold the text fields, if there were any
-  console.log(req.file)
-  res.end()
+  var file = req.file
+  res.jsonp({
+    filename: file.originalname,
+    size: file.size
+  })
 })
 
 

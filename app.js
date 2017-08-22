3ac48911b01cef4b7d025bd0994d11ca9d0eabf4
pullman14 <pitts.114@buckeyemail.osu.edu>
2017-08-21 20:28:10 -0400

    Print file contents to console

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
- 
+var express = require("express")
+var multer = require("multer")
+var storage = multer.memoryStorage()
+var upload = multer({storage: storage})
+
+var app = express()
+
+app.set('port', (process.env.PORT || 5000))
+
+app.post('/upload', upload.single('userFile'), function (req, res, next) {
+  // req.file is the `avatar` file
+  // req.body will hold the text fields, if there were any
+  console.log(req.file)
+  res.end()
+})
+
+
+app.use(express.static(__dirname + "/public"))
+
+app.listen(app.get("port"), function() {
+  console.log("Node app is running at http://localhost:" + app.get('port'))
+})
